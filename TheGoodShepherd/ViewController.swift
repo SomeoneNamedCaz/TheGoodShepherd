5de06400409e23ae5d15a3d0cb825b9ffe60dff8
cazcullimore <cazcullimore@gmail.com>
2023-11-18 09:45:10 -0700

    messing around with views and storyboard

--- TheGoodShepherd/ViewController.swift
+++ TheGoodShepherd/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var testLab: UILabel!
+    @IBOutlet var goalList: UIStackView!
+    var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+//        myLabel.text =
+//        testLab.text = "hi"
+//        testLab
         // Do any additional setup after loading the view.
+        
     }
-
-
+    @IBAction func triggerListener(_ sender: Any) {
+        if (view.backgroundColor == .black){
+            view.backgroundColor = .white
+        }else{
+            view.backgroundColor = .black
+        }
+        
+    }
+    
+    @IBSegueAction func showNextController(_ coder: NSCoder) -> UIViewController? {
+        return UIViewController(coder: coder)
+    }
+    
 }
 
---
+++ TheGoodShepherd/templeViewController.swift
@@ -0,0 +1,8 @@
+//
+//  templeViewController.swift
+//  TheGoodShepherd
+//
+//  Created by Caz Cullimore on 11/18/23.
+//
+
+import Foundation
